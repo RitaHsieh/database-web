ce2f3e436a9800ccf0d7e6d3d484db7d69f99530
danny <a7527744@gmail.com>
2022-10-19 15:40:24 +0800

    fix

--- component/Section.js
+++ component/Section.js
@@ -2,19 +2,14 @@ import style from "../styles/Section.module.css"
 
 export default function Section() {
     return(
-        <html>
-            <head>
-                <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet"/>
-            </head>
-            <body>
-                <div className={style.herocontainer}>
-                    <img className={style.background} src="background.jpg" alt='alter'/>
-                    <h1>Rent You on Sunday</h1>
-                    <div className="search-box"></div>
+        <>
+            <div className={style.herocontainer}>
+                <img className={style.background} src="background.jpg" alt='alter'/>
+                <h1>Rent You on Sunday</h1>
+                <div className="search-box"></div>
                     
                     
-                </div>
-            </body>
-        </html>
+            </div>
+        </>
     )
 }
--- pages/home.js
+++ pages/home.js
@@ -1,14 +1,14 @@
-import Briefintroduction from "../component/brief-introduction"
-import Section from "../component/Section"
-import Search from "../component/search"
+    import Briefintroduction from "../component/brief-introduction"
+    import Section from "../component/Section"
+    import Search from "../component/search"
 
-export default function Home() {
-    return (
-        <>
-            <Section/>
-            <Search/>
+    export default function Home() {
+        return ( 
+            <>
+                <Section/>
+                <Search/>
 
-            <Briefintroduction beforeintro="介紹" name="飯店名稱" intro="介紹" addr="地址" price="價格"/>
-        </>
-    )
-}
+                <Briefintroduction beforeintro="介紹" name="飯店名稱" intro="介紹" addr="地址" price="價格"/>
+            </>
+        )
+    }
