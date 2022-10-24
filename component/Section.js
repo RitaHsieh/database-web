117f232b01bca61f29e4feb087f30b59531aad2c
Evan <evanho0315@gmail.com>
2022-10-24 20:10:15 +0800

    add

--- component/Section.js
+++ component/Section.js
@@ -1,5 +1,5 @@
 import style from "../styles/Section.module.css"
-import Search from "../component/searchform"
+
 import Login from "../component/login"
 
 export default function Section({event}) {
@@ -8,7 +8,7 @@ export default function Section({event}) {
                 <div className={style.herocontainer}>
                     <img className={style.background} src="background.jpg" alt='alter'/>
                     <h1>Rent You on Sunday</h1>
-                    <Search/>
+                    
                     <Login/>
                     <button className={`${style.button} ${style.scrollindicator}`} onClick={event}>
                         <span className={style.scrollindicatorlabel}>introduction</span> 
