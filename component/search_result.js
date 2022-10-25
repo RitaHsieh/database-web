104c4ee489cbe3e780d506d195a4f4248a26f36a
Evan <evanho0315@gmail.com>
2022-10-25 15:56:58 +0800

    problem

--- component/search_result.js
+++ component/search_result.js
@@ -6,12 +6,16 @@ export default function search_result() {
         <>
             <div className={style.searchcontainer}>
                 <img src="image.jpg" alt="image" className={style.img}/> 
-                <h className={style.name}>旅館名稱(旅館英文名稱)</h>
-                <p>旅館地址</p>
-                <p>訂購房型</p>
-                <p>NT$價錢</p>
-                <p>住宿時間</p>
-                <p>訂單狀況</p>
+                <div>
+                    <h1 className={style.name}>旅館名稱(旅館英文名稱)</h1>
+                    <div>
+                        <p className={style.location}>旅館地址</p>
+                        <p className={style.room}>訂購房型</p>
+                    </div>
+                    <p>NT$價錢</p>
+                    <p>住宿時間</p>
+                    <p>訂單狀況</p>
+                </div>
             </div>
         </>
     )
