ea479259a83e34a8bdc2789d41d00abd1722328d
Evan <evanho0315@gmail.com>
2022-10-27 11:41:22 +0800

    add

--- component/Searchform.js
+++ component/Searchform.js
@@ -3,18 +3,25 @@ import style from "../styles/Searchform.module.css"
 export default function Search() {
     return(
         <>
-            <div className={style.searchcontainer}> 
+            <div className={style.searchcontainer}>
                 <p>地點</p>
                 <p>|</p>
-                <p>人數</p>
+                <select className={style.selectform} name="people" id="people"> 
+                    <option className={style.option} value="none" selected disabled hidden>人數</option> 
+                    <option className={style.option} value="one">&nbsp;&nbsp;1人</option> 
+                    <option className={style.option} value="two">&nbsp;&nbsp;2人</option> 
+                    <option className={style.option} value="three">&nbsp;&nbsp;3人</option> 
+                    <option className={style.option} value="four">&nbsp;&nbsp;4人</option> 
+                </select>
                 <p>|</p>
                 <p>入住時間</p>
                 <p>→</p>
                 <p>離開時間</p>
                 <p>|</p>
-                <a href="https://www.youtube.com/shorts/-hNK5kKW6HY">
-                    <input className={style.button} type="button" value="→)"/>
-                </a>
+                
+                    <a href="https://www.youtube.com/shorts/-hNK5kKW6HY">
+                        <input className={style.button} type="button" value="→)"/>
+                    </a>
             </div>
         </>
     )
