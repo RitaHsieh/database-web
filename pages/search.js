4b5fb9fa5a4956daf4346f03f349874bc31d1d19
Rita_Hsieh <ritasha356@gmail.com>
2022-10-30 18:43:37 +0800

    search page

--- pages/search.js
+++ pages/search.js
@@ -1,13 +1,9 @@
-import Order from "../component/order"
-import Filter from "../component/Filter"
-import Searchform_2 from "../component/Searchform-2"
+import Section from "../component/search_section"
 
 export default function login() {
     return (
         <>
-            <Order/>
-            <Searchform_2/>
-            <Filter/>
+            <Section/>
         </>
     )
 }
