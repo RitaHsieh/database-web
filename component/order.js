69b0fb23653cb06d990fc9da9682c236cec51e56
danny <a7527744@gmail.com>
2022-11-16 08:42:45 +0800

    fix

--- component/order.js
+++ component/order.js
@@ -3,14 +3,13 @@ import style from "../styles/order.module.css"
 export default function loginform() {
     return(
         <>
-            <div>
+            
             <select className={style.selectform} name="order" id="order"> 
                 <option className={style.option} value="none" selected disabled hidden>排序方式</option> 
                 <option className={style.option} value="price">價錢</option> 
                 <option className={style.option} value="stars">星級 </option> 
                 <option className={style.option} value="distance">距離</option> 
             </select>
-            </div>
         </>
     )
 }
