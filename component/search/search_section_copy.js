34f59923f671c42212cebc6430ac39f3320cd219
24hoshiiii <lessteyer12345@gmail.com>
2022-12-03 03:48:57 +0800

    lee

---
+++ component/search/search_section_copy.js
@@ -0,0 +1,43 @@
+import style from "../../styles/lee.module.css"
+import Filter from "./Filter"
+import { useState } from "react"
+
+export default function search_section_copy() {
+
+    const [float,setFloat] = useState(false);
+
+    const mask=[style.searchMask,style.searchMaskHidden];
+
+
+    const btnClick = () => {
+        setFloat(float => !float);
+    };
+
+    return (
+        <>
+            <div className={style.searchBottomContainer}>
+                <div className={style.bottomleftcontainer}/>
+                <div className={style.bottomrightcontainer}>
+                <div className={style.bottomrightcontainer_ex2}>
+                    <h1 className={style.title}>旅館名稱</h1>
+                    <br/>
+                    <p>地址：台北市大安區忠孝東路3段1號</p>
+                    <div className={style.collection}>收藏</div>
+                </div>
+                    <div className={style.bottomrightcontainer_ex}>
+                        <div className={style.bottomrightpicture}/>
+                        <div className={style.bottomrightpicture2}/>
+                        <div className={style.bottomrightpicture3}/>
+                        <div className={style.bottomrightpicture4}/>
+                        <div className={style.bottomrightpicture5}/>
+                        <div className={style.bottomrightpicture6}/>
+                    </div>
+                </div>
+            </div>
+                
+            <div className={float===true?mask[0]:mask[1]} onClick={event=>btnClick()}></div>
+            <Filter float={float===true?0:1}/>
+
+        </>
+    )
+}
--- pages/lee.js
+++ pages/lee.js
@@ -1,25 +1,9 @@
-import Left from "../component/hotelInfo/room"
-import Right from "../component/hotelInfo/book"
-import Evaluate from "../component/hotelInfo/evaluate"
-import Attraction from "../component/hotelInfo/attraction"
-import Information from "../component/hotelInfo/information"
-import Transportation from "../component/hotelInfo/transportation"
+import Search_section_copy from "../component/search/search_section_copy"
 
-export default function Home() {
+export default function login() {
     return (
         <>
-            <div style={{display:"flex"}}>
-                <div>
-                    <Left/>
-                    <Left/>
-                    <Left/>
-                </div>
-                <Right/>
-            </div>
-            <Evaluate/>
-            <Attraction/>
-            <Information/>
-            <Transportation/>
+            <Search_section_copy/>
         </>
     )
 }
