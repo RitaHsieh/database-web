f9ea0ff3aa1c4fba0775c37c79fb395cb627e702
24hoshiiii <lessteyer12345@gmail.com>
2022-12-02 18:26:41 +0800

    lee

---
+++ pages/lee.js
@@ -0,0 +1,25 @@
+import Left from "../component/hotelInfo/room"
+import Right from "../component/hotelInfo/book"
+import Evaluate from "../component/hotelInfo/evaluate"
+import Attraction from "../component/hotelInfo/attraction"
+import Information from "../component/hotelInfo/information"
+import Transportation from "../component/hotelInfo/transportation"
+
+export default function Home() {
+    return (
+        <>
+            <div style={{display:"flex"}}>
+                <div>
+                    <Left/>
+                    <Left/>
+                    <Left/>
+                </div>
+                <Right/>
+            </div>
+            <Evaluate/>
+            <Attraction/>
+            <Information/>
+            <Transportation/>
+        </>
+    )
+}
