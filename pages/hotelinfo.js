9b98ebcdc5def21100dcfcd8412cb7d152bac5d9
RitaHsieh <ritasha356@gmail.com>
2022-12-03 23:15:10 +0800

    hotelinfo sass

--- pages/hotelinfo.js
+++ pages/hotelinfo.js
@@ -1,3 +1,4 @@
+import Title from "../component/hotelInfo/title"
 import Left from "../component/hotelInfo/room"
 import Right from "../component/hotelInfo/book"
 import Evaluate from "../component/hotelInfo/evaluate"
@@ -5,17 +6,20 @@ import Attraction from "../component/hotelInfo/attraction"
 import Information from "../component/hotelInfo/information"
 import Transportation from "../component/hotelInfo/transportation"
 import style from "../styles/layout/home.module.css"
+
 export default function Home() {
     return (
         <>
+            {/* <Title/> */}
             <div className={style.hotelinfoSectionContainer}>
+                
                 <div className={style.roominfo}>
-                    <div>
-                        <Left/>
-                        <Left/>
-                        <Left/>
+                    <div className={style.left}>
+                        <Left className={style.left}/>
+                        <Left className={style.left}/>
+                        <Left className={style.left}/>
                     </div>
-                    <Right/>
+                    <Right className={style.right}/> 
                 </div>
                 <Evaluate/>
                 <Attraction/>
