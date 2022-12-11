729008df49f1101ca196d4b4f56573a5f95d995a
Evan <evanho0315@gmail.com>
2022-12-12 02:13:55 +0800

    sell/index_responsive_v1

--- component/user/sell/index-section.js
+++ component/user/sell/index-section.js
@@ -15,7 +15,6 @@ export default function Home() {
 
     return (
         <>
-            
             <Header/>
             <div className={style.userSellIndex_SettingContainer}>
                 <Menu/>
