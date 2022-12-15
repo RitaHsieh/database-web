2928b8d2390bc29ea7eba063537479529eea6025
Evan <evanho0315@gmail.com>
2022-12-15 14:10:27 +0800

    conflict_solve

--- component/hotelInfo/hotelinfo_component/roominfo_component/book.js
+++ component/hotelInfo/hotelinfo_component/roominfo_component/book.js
@@ -1,3 +1,12 @@
-import style from "../../styles/layout/home.module.css"
+import style from "../../../../styles/layout/home.module.css"
 
 export default function hotelinfo() {
+
+    return (
+        <>
+            <div className={style.hotelInfoBookContainer}>
+                <div className={style.right_container}><button type="button" className={style.button}>立即訂房</button></div>
+            </div>
+        </>
+    )
+}
