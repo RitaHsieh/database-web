f3545a43f94ca417dc5a9d68d5508aee4d4b174c
Evan <evanho0315@gmail.com>
2022-12-18 13:22:12 +0800

    Admin ID

---
+++ component/admin/multi/journalBlock.js
@@ -0,0 +1,9 @@
+import style from "../../../styles/layout/home.module.css"
+
+export default function ID() {
+    return (
+        <>
+            <p className={style.jbContainer}>JB</p>
+        </>
+    )
+}
