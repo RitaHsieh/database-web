ac140764cd642dac6653117cd6be27eff7289a47
RitaHsieh <ritasha356@gmail.com>
2022-12-27 21:38:49 +0800

    room fetch

--- component/user/hotel/additem.js
+++ component/user/hotel/additem.js
@@ -2,10 +2,11 @@
 import style from "../../../styles/layout/home.module.css";
 //@flow
 import React from 'react'
+import {useState} from 'react';
 
 
 export default function LabelItems(props) {
-    const name = props.name;
+
     return (
     <button className={style.added} type="button"> 
         <p>{props.name}</p>
--- component/user/hotel/room.js
+++ component/user/hotel/room.js
@@ -8,35 +8,95 @@ import Label from "./additem";
 
 import {useState} from 'react';
 import useSWR from "swr";
+import FormData from 'form-data';
 
 export default function room() {
 
+    // label 新增動畫
     const initialList=[];
-
     const [label, setLabel] = useState(true);
     const [list, setList] = useState(initialList);
     const [name, setName] = useState('');
-
     const label_input = [style.addLabel, style.addLabel_hide];
-
 
     const addOnClick = () => {
         setLabel(label => !label);
     };
     const handleChange = (e) => {
         setName(e.target.value);
-    }
-
+    };
     const handler = (e) => {
-
         if (e.key==='Enter'){
             setLabel(label => !label);
-            const newList = list.concat([{name: name, id: '1'},]);
+            const newList = list.concat([{name: name},]);
             setList(newList);
+            setInstallation(installation + "," + name);
             setName('');
         }
     };
 
+    // fetch api
+
+    const [room_name,setRoomName] = useState("");
+    const [quantity,setQuantity] = useState(0);
+    const [singleBed,setSingleBed] = useState(0);
+    const [doubleBed,setDoubleBed] = useState(0);
+    const [capacity,setCapacity] = useState(1);
+    const [introduction,setIntroduction] = useState("");
+    const [installation,setInstallation] = useState("");
+    const [original_price,setOriginalPrice] = useState(0);
+    const [price, setPrice] = useState(0);
+    const [shouldFetch,setShouldFetch] = useState(false);
+
+    const formData = new FormData();
+    formData.append('room_name', room_name);
+    formData.append('quantity', quantity);
+    formData.append('bed_type', singleBed.toString() + "," + doubleBed.toString());
+    formData.append('capacity', capacity);
+    formData.append('introduction', introduction);
+    formData.append('installation', installation);
+    formData.append('original_price', original_price);
+    formData.append('price', price);
+    formData.append('hotel_id', 1);
+    formData.append('images', "string");
+
+    const fetcher = (url,formData) => {fetch(url, {
+        headers: {
+                
+            },
+            body:formData,
+            method: 'POST'
+        })
+        .then((response) => {
+            setShouldFetch(shouldFetch => !shouldFetch);
+            return response.json();
+        })
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log("Error: ${error}");
+        })
+    }
+
+    const buttonClick=()=>{
+        // console.log("room_name: ", typeof(room_name))
+        // console.log("quantity: ", typeof(quantity))
+        // console.log("bed_type: ", typeof(singleBed.toString() + "," + doubleBed.toString()),singleBed.toString() + "," + doubleBed.toString() )
+        // console.log("capacity: ", typeof(capacity))
+        // console.log("introduction: ", typeof(introduction))
+        // console.log("installation: ", typeof(installation), installation)
+        // console.log("original_price: ", typeof(original_price))
+        // console.log("price: ", typeof(price))
+        for (const [key, value] of formData) {
+            console.log(key, value);
+        }
+        setShouldFetch(shouldFetch => !shouldFetch);
+        alert("Update");
+    }
+
+
+    const { data } = useSWR(shouldFetch ? ['https://dhkmj1jao2.execute-api.us-east-1.amazonaws.com/test/room/',formData] : null, fetcher)
 
     return (
         <>
@@ -46,40 +106,38 @@ export default function room() {
                 <Menu/>
                 <div className={style.main}>
                     <div className={style.content}>
-                        <h1>房型設定</h1>
+                        <h1>新增房型</h1>
                         <button className={style.hotelbutton}><Link href="/">預覽畫面</Link></button>
                     </div>
                     <form className={style.roomform}>
                         <div className={style.content}>
-                            <p>房型資訊</p>
-                            <input className={style.input} type="text"></input>
+                            <p>房型名稱</p>
+                            <input className={style.input} onChange={event=>{setRoomName(event.target.value)}} type="text"></input>
                         </div>
                         <div className={style.content}>
                             <p>可容納人數</p>
-                            <input className={style.input} type="text"></input>
+                            <input className={style.input} type="number" onChange={event=>{setCapacity(parseInt(event.target.value))}}></input>
                         </div>
                         <div className={style.content}>
                             <p>床型</p>
                             <div className={style.input}>
                                 <div className={style.content}>
-                                <input type="text"></input>
+                                <input type="number" onChange={event=>{setSingleBed(parseInt(event.target.value))}}></input>
                                 <p>張單人床</p>
                                 </div>
                                 <div className={style.content}>
-                                <input type="text"></input>
+                                <input type="number" onChange={event=>{setDoubleBed(parseInt(event.target.value))}}></input>
                                 <p>張雙人床</p>
                                 </div>
                             </div>
                         </div>
                         <p className={style.spacing}>房型介紹</p>
-                        <textarea className={style.textarea}></textarea>
+                        <textarea className={style.textarea} onChange={event=>{setIntroduction(event.target.value)}}></textarea>
                         <p>房間設施</p>
                         <div className={style.labelcontainer}>
-                            <ul>
-                            {list.map((item) => (    
+                            {list.map((item) => ( 
                                 <Label name={item.name}></Label>
                             ))}
-                            </ul>
                             <button className={style.add} type="button" onClick={event=>addOnClick()}> 
                                 <i class="bi bi-plus"></i>
                             </button>
@@ -88,21 +146,21 @@ export default function room() {
                             <input type="text" onKeyDown={(e) => handler(e)} onChange={(e) => handleChange(e)} value={name} placeholder="add label here"></input>
                         </div>
                         <div className={style.content}>
-                            <p>網站價</p><input className={style.input} type="text"></input>
+                            <p>網站價</p><input className={style.input} type="number" onChange={event=>{setPrice(parseInt(event.target.value))}}></input>
                         </div>
                         <div className={style.content}>
-                            <p>原價</p><input className={style.input} type="text"></input>
+                            <p>原價</p><input className={style.input} type="number" onChange={event=>{setOriginalPrice(parseInt(event.target.value))}}></input>
                         </div>
                         <div className={style.content}>
-                            <p>房型總數量</p><input className={style.input} type="text"></input>
+                            <p>房型總數量</p><input className={style.input} type="number" onChange={event=>{setQuantity(parseInt(event.target.value))}}></input>
                         </div>
                     </form>
-                    <div className={style.newroom}>
+                    {/* <div className={style.newroom}>
                         <i className="bi bi-plus"></i>
                         <p>新增房型</p>
-                    </div>
+                    </div> */}
                     <div className={style.btn_content}>
-                        <button className={style.btn}><Link href="/user/hotel/">確認</Link></button>
+                        <button className={style.btn} onClick={event=>buttonClick()}>確認</button>
                     </div>
                 </div>
             
