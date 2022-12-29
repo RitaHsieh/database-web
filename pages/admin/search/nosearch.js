8abaa728ef430257a3a238ce1feef562f3367b23
Evan <evanho0315@gmail.com>
2022-12-29 13:46:06 +0800

    noSearchResult

---
+++ pages/admin/search/nosearch.js
@@ -0,0 +1,9 @@
+import NoSearch from "../../../component/admin/section/nosearch-section"
+
+export default function AdminNoSearch() {
+    return (
+        <>
+            <NoSearch/>
+        </>
+    )
+}
