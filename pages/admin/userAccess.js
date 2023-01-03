9a99fbb92ddf316c6d8af973455dfa2ada22d028
Evan <evanho0315@gmail.com>
2023-01-03 18:40:17 +0800

    deleteButton

---
+++ pages/admin/userAccess.js
@@ -0,0 +1,9 @@
+import UserAccess from "../../component/admin/section/userAccess-section"
+
+export default function AdminUserAccess() {
+    return (
+        <>
+            <UserAccess/>
+        </>
+    )
+}
